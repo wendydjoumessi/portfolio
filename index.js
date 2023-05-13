0ddb6dd119bef0400322c0bd726244876d0b3261
wendydjoumessi <wendydjoumessi112@gmail.com>
2023-05-14 01:51:17 +0200

    this is the first part of my application.

---
+++ index.js
@@ -0,0 +1,5 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import APP from './APP';
+ReactDOM.render(<APP/> , document.getElementById('root')); //THIS LINE INDICATE THAT
+// ALL THE CONTENT OF THIS FILE SHOULD BE RENDERED IN THE ROOT ID OF THE HTML FILE
