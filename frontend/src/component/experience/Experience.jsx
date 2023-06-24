7c4a291c43070bbc78aa39f7fd17171394b62a7e
wendydjoumessi <wendydjoumessi112@gmail.com>
2023-06-24 10:49:54 +0200

    update

--- frontend/src/component/experience/Experience.jsx
+++ frontend/src/component/experience/Experience.jsx
@@ -28,15 +28,15 @@ const Experience = () => {
         {
           experienceData.map((expi, index) => {
             return(
-              <div className="experience_frontend">
+              <div className="experience__frontend">
                 <h3>{expi.name}</h3> 
-                <div className="experience_content">
+                <div className="experience__content">
                   {
                     expi.experience.map((skill, index)=>{
                       return (
                       
-                          <article className="experience_details">
-                            <MdVerified className="experience_details-icon" />
+                          <article className="experience__details">
+                            <MdVerified className="experiensce__details-icon" />
                             <div>
                               <h4>{skill.skillName}</h4>
                               <small className="text-light">{skill.skillLevel}</small>
